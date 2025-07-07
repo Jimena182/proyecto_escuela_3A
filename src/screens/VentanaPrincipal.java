a6897ed3858d7464bc056c6016bd05cac204fbaf
Jimena182 <baploru@gmail.com>
2025-07-07 09:43:14 -0600

    Adding menuBar

---
+++ src/screens/VentanaPrincipal.java
@@ -0,0 +1,39 @@
+package screens;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class VentanaPrincipal extends JFrame {
+
+    private JMenuBar menuBar; //Barra menu
+    private JMenu menuAlumnos; //menu Alumno
+    private JMenuItem menuInsertarAlumnos; //Elemento Insertar
+    
+    public VentanaPrincipal(String title){
+        this.setTitle(title);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        inicomponents();
+    }
+    
+    private void inicomponents(){ 
+        //Barra de menu
+        menuBar = new JMenuBar();
+        
+        //menu Alumno
+        menuAlumnos = new JMenu();
+        menuAlumnos.setText("Alumnos");
+        
+        //Menu insertar
+        menuInsertarAlumnos = new JMenuItem();
+        menuInsertarAlumnos.setText("Insertar...");
+        //agregando menus
+        menuAlumnos.add(menuInsertarAlumnos);
+        menuBar.add(menuAlumnos);
+        this.setJMenuBar(menuBar);
+        
+        pack();
+
+}
+}
